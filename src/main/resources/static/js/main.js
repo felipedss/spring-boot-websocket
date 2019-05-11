7beefd84234ae6322809db9da8437a5320f27c49
Felipe da Rosa <felipedss@hotmail.com>
2019-05-11 15:06:26 -0300

    traduzindo para português

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -74,10 +74,10 @@ function onMessageReceived(payload) {
 
     if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
+        message.content = message.sender + ' entrou!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+        message.content = message.sender + ' saiu!';
     } else {
         messageElement.classList.add('chat-message');
 
